510f6ef67d19c5784267f5cb3b9c54b75058c339
Celso Netto <celso.bmnetto1@gmail.com>
2024-05-12 01:18:46 -0300

    Main melhor

--- Main.java
+++ Main.java
@@ -1,18 +1,36 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        double x0 = 0; // Valor inicial de x
-        double y0 = 1; // Valor inicial de y
-        double h = 0.1; // Tamanho do passo
-        double xf = 1; // Valor final de x
-        Euler euler = new Euler(x0, y0, h, xf);
-        
-        double resultado = euler.resolverEuler(x0, y0, h, xf);
+        double resultado;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Valor inicial de X: ");
+        double x0 = sc.nextDouble(); // Valor inicial de x
 
+        System.out.println("Valor inicial de Y: ");
+        double y0 = sc.nextDouble(); // Valor inicial de y
+
+        System.out.println("Tamanho do passo: ");
+        double h = sc.nextDouble(); // Tamanho do passo
+
+        System.out.println("Valor final de X: ");
+        double xf = sc.nextDouble(); // Valor final de x
+
+        //Resolve a equação de três formas diferentes
+        Euler euler = new Euler(x0, y0, h, xf);
+        resultado = euler.resolverEuler(x0, y0, h, xf);
         System.out.println("Resultado usando o método de Euler: " + resultado);
+
+        EulerModificado eulerModificado = new EulerModificado(x0, y0, h, xf);
+        resultado = eulerModificado.resolverEulerModificado(x0, y0, h, xf);
+        System.out.println("Resultado usando o método de Euler Modificado: " + resultado);
+
+        RungeKupta rungeKupta = new RungeKupta(x0, y0, h, xf);
+        resultado = rungeKupta.resolverRungeKupta(x0, y0, h, xf);
+        System.out.println("Resultado usando o método de Runge-Kupta: " + resultado);
     }
 }
 
