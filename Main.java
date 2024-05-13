975703011e1eb68056083136c13d30611e56804a
Celso Netto <celso.bmnetto1@gmail.com>
2024-05-12 21:51:25 -0300

    completo

--- Main.java
+++ Main.java
@@ -1,7 +1,5 @@
 public class Main {
     public static void main(String[] args) throws Exception {
-        double resultado;
-
         //condicoes iniciais
         double x0 = 0;
         double y0 = 1;
@@ -10,29 +8,45 @@ public class Main {
         //valor que quer se aproximar
         double x = 0.1;
         
-
-        //Resolve a equação de três formas diferentes
+        //Resolve a equação de duas formas diferentes
         Euler(x0,y0,h,x);
-        //EulerModificado(x0,n,tf);
-        //RungeKupta(x0,n,tf);
+        RungeKutta(x0,y0,h,x);
     }
 
-    // dy/dx=(x + y + xy)
     static double funcao(double x, double y) {
         return (x + y + x * y);
     }
 
     static void Euler(double x0, double y, double h, double x) {
-        double temp;
-
         // Iterar ate onde tem a aproximacao
         while (x0 < x) {
-            temp = y;
             y = y + h * funcao(x0, y);
             x0 = x0 + h;
         }
+
+        System.out.println("O valor de Y quando X = " + x + " é " + y);
+    }
+
+    static void RungeKutta(double x0, double y0, double h, double x) {
+        //num interacoes
+        int n = (int)((x - x0) / h);
+        double k1, k2, k3, k4;
+        double y = y0;
+
+        for (int i = 0; i < n; i++) {
+            //Achar o proximo valor de y
+            k1 = h * (funcao(x0, y));
+            k2 = h * (funcao(x0 + 0.5 * h, y + 0.5 * k1));
+            k3 = h * (funcao(x0 + 0.5 * h, y + 0.5 * k2));
+            k4 = h * (funcao(x0 + h, y + k3));
  
-        // Printing approximation
-        System.out.println("Solução aproximada quando X = " + x + " é " + y);
+            //Atualiza y
+            y = y + (1.0 / 6.0) * (k1 + 2 * k2 + 2 * k3 + k4);
+             
+            //Atualiza x
+            x0 = x0 + h;
+        }
+
+        System.out.println("O valor de Y quando X = " + x + " é " + y);
     }
 }
